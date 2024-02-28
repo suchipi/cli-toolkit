3b5745352382a003cc1da71d20d4c335fa2b6f41
Lily Skye <me@suchipi.com>
2024-02-27 23:00:40 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { runMain } from "@suchipi/run-main";
+export * from "@suchipi/string-utils";
+export { Hint, parseArgv } from "clef-parse";
+export { Path } from "nice-path";
+export * as t from "pheno";
